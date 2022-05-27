72486c0369896a42daea510e2604e311ba3a25f7
skqnd3r <alexandre.adm3@gmail.com>
2022-05-27 04:21:59 -0500

    entity in array

--- source/main.c
+++ source/main.c
@@ -13,28 +13,41 @@ int main()
     };
     SDL_SetRenderDrawColor(app->renderer, 96, 128, 255, 255); // Modifier couleur fond
     
-    // spawn obstacle
-    // Entity *obstacle = spawnObstacle();
+    // init funciton
+    Entity *entities[10];
+    entities[0] = spawnPlayer(app);
+    for(int i=1;i!=10;i++){
+        entities[i] = spawnObstacle(app);
+    }
     
 
-    Entity *player = initPlayer(app);
-    Entity *obstacle = spawnObstacle(app);
 
-    while(input_handler(player) == 0){
-        // clear screen
-        prepareCanvas(app);
-        
-        // refresh pos
-        drawEntity(app, obstacle);
-        drawEntity(app, player);
+    // animation
+    // int counter =5;
 
-        //draw collider
-        drawCollider(app,obstacle);
-        drawCollider(app,player);
+    while(input_handler(entities[0]) == 0){
+        entities[1]->hide=0;
+        Move(entities);
+        Refresh(app,entities);
 
-        //print screen
-        presentCanvas(app);
+        // // factory
+        // if(null) {
+
+        //     int random()
+        // }
+        // else if (count == )
+        // {
+        //     spawnObstacl
+        //     null
+        // }
+        // i -=1
+
         SDL_Delay(16);
     }
+
+    for(int i=0;i!=10;i++){
+        free(entities[i]);
+    }
+
     return 0;
 }
