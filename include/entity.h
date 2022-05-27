8da172748d37c208db7d1b4ce5945512227a8ab2
skqnd3r <alexandre.adm3@gmail.com>
2022-05-27 04:23:32 -0500

    add collider and movement

--- include/entity.h
+++ include/entity.h
@@ -5,18 +5,27 @@
 #include LIB_SDL_IMG
 
 typedef struct {
+    SDL_Texture *texture;
     int pos_x;
     int pos_y;
     int width;
     int height;
-    SDL_Texture *texture;
     SDL_Rect collider;
+    int life;
+    float speed;
+    int mv_x;
+    int mv_y;
+    int hide;
 } Entity;
 
 #include "window.h"
 #include "draw.h"
 
-Entity *initPlayer(Win *app);
+Entity *spawnPlayer(Win *app);
 Entity *spawnObstacle(Win *app);
+
+SDL_Rect setCollider(Entity *entity);
+void Move(Entity **entities);
+
 
 #endif
