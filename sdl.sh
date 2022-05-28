ce6cf7e292dd1eca6667b5580b24e986a546c73f
skqnd3r <alexandre.adm3@gmail.com>
2022-05-28 08:31:39 -0500

    install switch mac/lin

--- sdl.sh
+++ sdl.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-if [ "$1" == "-w" ]; then
-    sudo apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev -y
-elif [ "$1" == "-m" ]; then
-    brew install libsdl2-dev libsdl2-image-dev -y
+if [ "$1" == "lin" ]; then
+    if [ "$2" == "-b" ]; then
+        mv Makefile ./lin 
+    else
+        mv lin/Makefile ./
+        if [ "$2" == "-d" ]; then
+            sudo apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev -y
+        fi
+    fi
+elif [ "$1" == "mac" ]; then
+    if [ "$2" == "-b" ]; then
+        mv Makefile ./mac
+    else
+        mv mac/Makefile ./
+        if [ "$2" == "-d" ]; then
+            brew install libsdl2-dev libsdl2-image-dev -y
+        fi
+    fi
 fi
