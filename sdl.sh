9b50b9a09419ef742fe5a45c1e256d7f4f0598c6
skqnd3r <alexandre.adm3@gmail.com>
2022-05-28 09:33:22 -0500

    change dir name

--- sdl.sh
+++ sdl.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 if [ "$1" == "lin" ]; then
     if [ "$2" == "-b" ]; then
-        mv Makefile ./lin 
+        mv Makefile ./_lin 
     else
-        mv lin/Makefile ./
+        mv _lin/Makefile ./
         if [ "$2" == "-d" ]; then
             sudo apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev -y
         fi
     fi
 elif [ "$1" == "mac" ]; then
     if [ "$2" == "-b" ]; then
-        mv Makefile ./mac
+        mv Makefile ./_mac
     else
-        mv mac/Makefile ./
+        mv _mac/Makefile ./
         if [ "$2" == "-d" ]; then
             brew install libsdl2-dev libsdl2-image-dev -y
         fi
